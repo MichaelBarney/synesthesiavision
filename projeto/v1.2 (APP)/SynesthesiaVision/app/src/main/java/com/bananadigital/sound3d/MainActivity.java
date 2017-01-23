45fc799fe159478ae082b84a4507fcdb19524f5f
KilnerJhow <jonathankilner@hotmail.com>
2017-01-23 20:23:19 -0300

    Adicionado comandos de voz

--- projeto/v1.2 (APP)/SynesthesiaVision/app/src/main/java/com/bananadigital/sound3d/MainActivity.java
+++ projeto/v1.2 (APP)/SynesthesiaVision/app/src/main/java/com/bananadigital/sound3d/MainActivity.java
@@ -1,5 +1,6 @@
 package com.bananadigital.sound3d;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.media.AudioAttributes;
 import android.media.SoundPool;
@@ -7,6 +8,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -14,10 +16,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -62,12 +67,16 @@ public class MainActivity extends AppCompatActivity {
     private float ev;
     private float dv;
 
+    private ImageButton btnSpeak;
+    private TextView txtSpeechInput;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        writeHandler = ConnectBluetooth.btt.getWriteHandler();
-        ConnectBluetooth.btt.setReadHandler(readHandler);
+        //writeHandler = ConnectBluetooth.btt.getWriteHandler();
+        //ConnectBluetooth.btt.setReadHandler(readHandler);
         setContentView(R.layout.activity_main);
 
         //sound
@@ -119,7 +128,17 @@ public class MainActivity extends AppCompatActivity {
 
         tempo_sensor = (TextView) findViewById(R.id.tempo_sensor);
         tempo_total = (TextView) findViewById(R.id.tempo_total);
+        btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
+        txtSpeechInput = (TextView) findViewById(R.id.txtSpeechInput);
 
+
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                promptSpeechInput();
+            }
+        });
 
         SeekBar frequency = (SeekBar) findViewById(R.id.frequency);
         frequency.setMax(TIME_MAX);
@@ -163,6 +182,53 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
     }
+
+
+    /**
+     * Showing google speech input dialog
+     * */
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Receiving speech input
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && data != null) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txtSpeechInput.setText(result.get(0));
+
+                    if(result.contains("iniciar")) {
+                        Toast.makeText(this, "Iniciado", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+                break;
+            }
+
+        }
+    }
+
 
     private void stopTimer() {
         if(timer != null && task != null) {
